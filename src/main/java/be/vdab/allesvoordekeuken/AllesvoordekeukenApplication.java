49b615fcf32d9df72a96feba2ef931f5313f9f47
Turiaf Kouakanou <turiaf@gmail.com>
2020-01-27 10:41:07 +0100

    start taken

---
+++ src/main/java/be/vdab/allesvoordekeuken/AllesvoordekeukenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.allesvoordekeuken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllesvoordekeukenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllesvoordekeukenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/allesvoordekeuken/AllesvoordekeukenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.allesvoordekeuken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllesvoordekeukenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
